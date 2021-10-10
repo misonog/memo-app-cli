9f04fb26d940a74360511617bd55a1f9909c0d34
misono <ginpeimisonou@gmail.com>
2021-10-10 12:34:35 +0900

    メモ削除機能の追加

--- main.js
+++ main.js
@@ -3,5 +3,5 @@
 const argv = require('minimist')(process.argv.slice(2))
 const Command = require('./lib/command')
 
-const command = new Command(argv.l, argv.r)
+const command = new Command(argv.l, argv.r, argv.d)
 command.run()
